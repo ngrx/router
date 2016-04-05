aeee2815e129c525c89d2c2132d6b52a3c2ea9a7
Mike Ryan <mike.ryan52@gmail.com>
2016-04-05 16:11:08 -0500

    feat(Params): Simplify params services by removing select method
    Remove the select method from RouteParams and QueryParams. Simplify both services
    
    by giving them an abstract token for DI and a simple projection factory. Remove
    
    the replace method from QueryParams. Add new search method to Location to update
    
    query params.
    
    
    
    BREAKING: select method removed from QueryParams and RouteParams. Use pluck instead:
    
    
    
      BEFORE:
    
    
    
      ```ts
    
      routeParams.select('id').subscribe(id => {});
    
      ```
    
    
    
      AFTER:
    
    
    
      ```ts
    
      routeParams.pluck('id').subscribe(id => {});
    
      ```

--- lib/index.ts
+++ lib/index.ts
@@ -3,7 +3,6 @@ import { LocationStrategy } from 'angular2/src/router/location/location_strategy
 import { PathLocationStrategy } from 'angular2/src/router/location/path_location_strategy';
 
 import { LOCATION_PROVIDERS } from './location';
-import { ROUTE_PARAMS_PROVIDERS } from './route-params';
 import { ROUTE_SET_PROVIDERS } from './route-set';
 import { ROUTE_VIEW_PROVIDERS } from './route-view';
 import { REDIRECT_PROVIDERS } from './redirect';
@@ -11,25 +10,24 @@ import { ROUTES, Routes } from './route';
 import { GUARD_PROVIDERS } from './guard';
 import { MATCH_ROUTE_PROVIDERS } from './match-route';
 import { COMPONENT_RENDERER_PROVIDERS } from './component-renderer';
-import { QUERY_PARAMS_PROVIDERS } from './query-params';
 import { LINK_TO_PROVIDERS } from './link-to';
 import { LINK_ACTIVE_PROVIDERS } from './link-active';
+import { PARAMS_PROVIDERS } from './params';
 
 export function provideRouter(routes: Routes) {
   return [
     provide(LocationStrategy, { useClass: PathLocationStrategy }),
     provide(ROUTES, { useValue: routes }),
     LOCATION_PROVIDERS,
-    ROUTE_PARAMS_PROVIDERS,
     ROUTE_SET_PROVIDERS,
     ROUTE_VIEW_PROVIDERS,
     REDIRECT_PROVIDERS,
     GUARD_PROVIDERS,
     MATCH_ROUTE_PROVIDERS,
     COMPONENT_RENDERER_PROVIDERS,
-    QUERY_PARAMS_PROVIDERS,
     LINK_TO_PROVIDERS,
-    LINK_ACTIVE_PROVIDERS
+    LINK_ACTIVE_PROVIDERS,
+    PARAMS_PROVIDERS
   ];
 }
 
@@ -37,11 +35,10 @@ export function provideRouter(routes: Routes) {
 export { Guard, createGuard } from './guard';
 export { LocationChange, Location } from './location';
 export { Middleware, createMiddleware } from './middleware';
-export { RouteParams } from './route-params';
+export { RouteParams, QueryParams } from './params';
 export { useLocationMiddleware, useRouteSetMiddleware, RouteSet, NextRoute } from './route-set';
 export { usePreRenderMiddleware, usePostRenderMiddleware, RenderInstruction } from './component-renderer';
 export { Routes, Route, IndexRoute } from './route';
 export { useTraversalMiddleware, TraversalCandidate } from './match-route';
-export { QueryParams } from './query-params';
 export { LinkTo } from './link-to';
 export { LinkActive } from './link-active';
