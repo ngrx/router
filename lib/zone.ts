63dda4153affab4cd2719b9e71f901c5412ad233
Mike Ryan <mike.ryan52@gmail.com>
2016-04-25 10:52:30 -0500

    fix(RouterInstruction): Pass the subscriber back through NgZone

---
+++ lib/zone.ts
@@ -0,0 +1,22 @@
+import { NgZone } from 'angular2/core';
+import { Operator } from 'rxjs/Operator';
+import { Subscriber } from 'rxjs/Subscriber';
+
+
+export class ZoneOperator<T> implements Operator<T, T> {
+  constructor(private _zone: NgZone) { }
+
+  call(subscriber: Subscriber<T>): Subscriber<T> {
+    return new ZoneSubscriber(subscriber, this._zone);
+  }
+}
+
+class ZoneSubscriber<T> extends Subscriber<T> {
+  constructor(destination: Subscriber<T>, private _zone: NgZone) {
+    super(destination);
+  }
+
+  protected _next(value: T) {
+    this._zone.run(() => this.destination.next(value));
+  }
+}
