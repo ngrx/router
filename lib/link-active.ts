a37962736738515c6af758e915d926cff94bad60
Brandon <robertsbt@gmail.com>
2016-06-06 07:35:46 -0500

    feat(LinkActive): Added override of default activeOptions for linkActive directive (#118)

--- lib/link-active.ts
+++ lib/link-active.ts
@@ -6,7 +6,10 @@ import {
   OnDestroy,
   Query,
   QueryList,
-  Renderer
+  Renderer,
+  Optional,
+  OpaqueToken,
+  Inject
 } from '@angular/core';
 import { LinkTo } from './link-to';
 import { Router } from './router';
@@ -14,6 +17,10 @@ import { Router } from './router';
 export interface LinkActiveOptions {
   exact: boolean;
 }
+
+export const LINK_ACTIVE_OPTIONS: LinkActiveOptions = {
+  exact: true
+};
 
 /**
  * The LinkActive directive toggles classes on elements that contain an active linkTo directive
@@ -28,17 +35,27 @@ export interface LinkActiveOptions {
  @Directive({ selector: '[linkActive]' })
  export class LinkActive implements AfterViewInit, OnDestroy {
    @Input('linkActive') activeClass: string = 'active';
-   @Input() activeOptions: LinkActiveOptions = { exact: true };
+   @Input() activeOptions: LinkActiveOptions;
    private _sub: any;
+   private _activeOptions: LinkActiveOptions = { exact: true };
 
    constructor(
      @Query(LinkTo) public links: QueryList<LinkTo>,
      public element: ElementRef,
      public router$: Router,
-     public renderer: Renderer
+     public renderer: Renderer,
+     @Optional()
+     @Inject(LINK_ACTIVE_OPTIONS)
+      private defaultActiveOptions: LinkActiveOptions
    ) {}
 
    ngAfterViewInit() {
+     if (this.defaultActiveOptions && !this.activeOptions) {
+       this._activeOptions = this.defaultActiveOptions;
+     } else if (this.activeOptions) {
+       this._activeOptions = this.activeOptions;
+     }
+
      this._sub = this.router$
      .map(({path}) => this.router$.prepareExternalUrl(path || '/'))
      .subscribe(path => {
@@ -50,7 +67,7 @@ export interface LinkActiveOptions {
     let active = this.links.reduce((active, current) => {
       let [href, query] = current.linkHref.split('?');
 
-      if (this.activeOptions.exact) {
+      if (this._activeOptions.exact) {
         return active ? active : href === path;
       } else {
         return active ? active : path.startsWith(href);
